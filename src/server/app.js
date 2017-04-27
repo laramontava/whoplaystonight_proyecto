5cbb5963490e053005e67538d9d718a58b1a7a44
laramontava <laramontava@gmail.com>
2017-04-27 02:29:12 +0200

    Now build run in port 80

--- src/server/app.js
+++ src/server/app.js
@@ -4,7 +4,7 @@
 var fs = require('fs');
 var https = require('https');
 var http = require('http');
-// var forceSSL = require('express-force-ssl');
+var forceSSL = require('express-force-ssl');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -62,17 +62,17 @@ switch (environment) {
     // Any deep link calls should return index.html
     app.use('/*', express.static('./build/index.html'));
     console.log('WARNING: OPEN BROWSER WITH HTTPS');
-
+    
     https.createServer({
       key: fs.readFileSync('server.key'),
       cert:fs.readFileSync('server.crt')
     }, app).listen(port);
 
-    // app-use(forceSSL);//MODULE USED TO FORCE REDIRECTION
-    // console.log('WARNING: BE CAREFULL, WE ARE TRYING TO LAUNCH SERVER ON PORT 80.' +
-    //             'CHECK IF ANY OTHER SERVER IS LISTENING ON SAME PORT (APACHE...)' +
-    //             'WE WANT TO FORCE HTTP TO HTTPS REDIRECTION ALWAYS');
-    // http.createServer(app).listen(80);
+     app.use(forceSSL);//MODULE USED TO FORCE REDIRECTION
+     console.log('WARNING: BE CAREFULL, WE ARE TRYING TO LAUNCH SERVER ON PORT 80.' +
+                 'CHECK IF ANY OTHER SERVER IS LISTENING ON SAME PORT (APACHE...)' +
+                 'WE WANT TO FORCE HTTP TO HTTPS REDIRECTION ALWAYS');
+     http.createServer(app).listen(80);
     break;
   default:
     console.log('** DEV **');
